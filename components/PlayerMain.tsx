c3ed3f62587d2a368cba4c3e262b8bbc1a56f9bb
sercan-works <sercantas1026@gmail.com>
2022-12-08 02:55:37 +0300

    x

---
+++ components/PlayerMain.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+
+const playList = [  {
+    name: 'name',
+    writer: 'writer',
+    img: 'image.jpg',
+    src: 'https://file-examples.com/storage/fe352586866388d59a8918d/2017/11/file_example_MP3_700KB.mp3',
+    id: 1,
+  },
+]
+
+
+
+const PlayerMain = () => {
+
+  return (
+
+    <div className='absolute bottom-10 left-10 ring-2 ring-gray-500 border-4 rounded-xl '>
+{/* <div> */}
+
+
+   
+    </div>
+    
+  )
+}
+
+export default PlayerMain
